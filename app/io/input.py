746da4d132401c025417ac8ea89cb3e890beea4e
Влад Виговський <mivlad612@gmail.com>
2025-04-08 17:41:22 +0300

    realized I\O functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,9 +1,38 @@
+import pandas
 
 def input_from_console():
-    pass
+    """
+    Prompts the user for text via console input..
 
-def read_file():
-    pass
+    Returns:
+        str: Text entered by the user.
+    """
+    text = input("Enter a text: ")
+    return text
 
-def read_file_pandas():
-    pass
+def read_file(file_path):
+    """
+    Reads text from a file at the specified path using standard Python tools.
+
+    Args:
+        file_path (str): File path.
+
+    Returns:
+        str: File contents as a string.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        text = file.read()
+    return text
+
+def read_file_pandas(file_path):
+    '''
+    Reads text from a file at the specified path using pandas.
+
+    Args:
+        file_path (str): File path.
+
+    Returns:
+        pandas.DataFrame: File contents as a dataframe.
+    '''
+    text = pandas.read_csv(file_path, encoding="utf-8")
+    return text
--- app/io/output.py
+++ app/io/output.py
@@ -1,9 +1,20 @@
 
-def output_in_console():
-    pass
+def output_in_console(text):
+    '''
+    Prints text to the console.
 
-def write_in_file():
-    pass
+    Args:
+        text(str): Text to be printed.
+    '''
+    print(text)
 
-def write_in_file_pandas():
-    pass
+def write_in_file(file_path, text):
+    '''
+    Writes text to a file.
+
+    args:
+        file_path (str): Path to the file.
+        text (str): Text to be written.
+    '''
+    with open(file_path, 'w', encoding="utf-8") as f:
+        f.write(text)
